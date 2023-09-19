439ec18a0405928ecf6bec356c8beb67dacd785a
andy <andy.yu617@gmail.com>
2023-09-19 00:18:48 +0000

    add font styling

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,12 +1,12 @@
-import styles from './styles.module.css';
-import Socials from '../Socials';
+import styles from "./styles.module.css";
+import Socials from "../Socials";
 
 export default function Contact() {
   return (
     <footer className={`container ${styles.contact}`}>
       <div>
-        <h2>Contact me on my socials:</h2>
-        <p>Let's set up a coffee chat</p>
+        <h4>Contact me on my socials:</h4>
+        <p>Let's set up a coffee chat!</p>
       </div>
       <div className={styles.socials}>{Socials}</div>
     </footer>
